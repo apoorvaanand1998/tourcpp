db241d0a0807b1385b69d0a2d96d911ab5d8b321
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-06-23 05:38:05 +0530

    Use auto

---
+++ dcl.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  auto i = 42;
+  auto f = 42.42;
+  auto c = 'a';
+  auto s = "You can do it AA!";
+
+  cout << i << f << c << s;
+  return 0;
+}
