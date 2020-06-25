d143377f98030d672274ca9c67e2c59ad11adc21
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-06-25 13:10:46 +0530

    Implement (not really) enum_classes. Also, use std library variant instead of union

---
+++ enum_classes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+enum class Color { red, blue, green };
+enum class Traffic_light { green, yellow, red };
+
+int main()
+{
+  Traffic_light tl = Traffic_light::red;
+  Color c = Color::red;
+
+  // wish I could print these values but idk enough yet lol
+  return 0;
+}
