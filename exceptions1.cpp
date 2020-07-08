86e2d26a796e6df7188479016506b925e1157765
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-07-09 04:34:18 +0530

    Implement exception handling with stdexcept and std errors

---
+++ exceptions1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+class Vector {
+public:
+  Vector(int s);
+  double& operator[](int i);
+  int size() noexcept;
+private:
+  double* elem;
+  int sz;
+};
+
+Vector::Vector(int s) // implementing a class invariant
+{
+  if (s < 0) {
+    throw length_error("Constructor: Negative Size");
+  }
+  elem = new double[s];
+  sz = s;
+}
+
+double& Vector::operator[](int i)
+{
+  if ((i < 0) || (i >= size())) {
+    throw out_of_range("Vector::operator[]");
+  }
+  return elem[i];
+}
+
+int Vector::size() noexcept // function guarantees there'll be no error
+{
+  return sz;
+}
+
+int main()
+{
+  Vector v(4);
+
+  try {
+    cout << v[5];
+    // should cause out_of_range error
+  }
+  catch (out_of_range& err) {
+    cerr << err.what() << '\n';
+  }
+  return 0;
+}
